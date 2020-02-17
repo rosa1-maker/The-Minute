80a3419d109cd328c4e17afb3cdd8ea74a61cf4c
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-17 15:32:51 +0300

    created an @migrate command in mangage.py and a manager command for test, also a manager shell

--- manage.py
+++ manage.py
@@ -5,11 +5,27 @@ from model_user import User
 from model_comment import Comment
 
 
-app = create_app()
+app = create_app('/')
 
 manager = Manager(app)
 manager.add_command('server', Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    '''
+    It will run the unit test
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 @manager .shell
 def make_shell_context():
-    return dict(app)ict(app)
+    return dict(app=app, db=db, User=user, Comment=comment, PitchCategory=PitchCategory, Pitches=Pitches)
+    pass
+
+if __name__ == '__main__':
+    manager.run()
