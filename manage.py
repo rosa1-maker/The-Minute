4b3560ba87bb47cfd458827ce5079b207943543f
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-17 15:20:41 +0300

    imported migrate and manager in manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,14 @@
 from app import create_app,db
-from model_user import model_user
+from flask_script import Manager, Server
+from flask_migrate import MigrateCommand, Migrate
+from model_user import User
+from model_comment import Comment
+
+
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('server', Server)
 
 @manager .shell
 def make_shell_context():
