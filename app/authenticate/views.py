176261896484cf4c6f659483834ce8e5103f56ef
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-17 14:47:07 +0300

    installed SQLAlchemy and creaated a function for registration in viiews.py  of the authentuucate folder

--- app/authenticate/views.py
+++ app/authenticate/views.py
@@ -17,3 +17,16 @@ def login():
 
         flash('Invalid username or password')
     return render_template('authenticate/login_form,', login_form=login_form)    
+
+@authenticate.route('/register', methods=['GET', 'POST'])
+def register():
+    form= RegistrationForm():
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('authenticate.login'))
+
+    return render_template('authenticate/register.html', RegistrationForm = form)    
+
---
+++ manage.py
@@ -0,0 +1,6 @@
+from app import create_app,db
+from model_user import model_user
+
+@manager .shell
+def make_shell_context():
+    return dict(app)ict(app)
