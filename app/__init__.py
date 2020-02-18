8f390c8424e4850637f813f3063fe3473ad6a668
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-18 07:55:32 +0300

    installed requirements.txt, flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from app.main import main as main_blueprint
+from app.authenticate import authenticate as authenticate_blueprint
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
@@ -7,21 +9,23 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
 from flask_moment import Moment
 
+
 def create_app(config_name):
 
     app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
-	bootstrap.init_app(app)
-	db.init_app(app)
-	login_manager.init_app(app)
-	moment.init_app(app)
-	login_manager.session_protection = 'strong'
-	login_manager.login_view = 'authenticate.login'
-	configure_uploads(app,photos)
-	mail.init_app(app)
 
-from app.main import main as main_blueprint
+    ## initializing bootsrap
+bootstrap.init_app(app)
+db.init_app(app)
+login_manager.init_app(app)
+moment.init_app(app)
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'authenticate.login'
+configure_uploads(app, photos)
+mail.init_app(app)
+
+
 app.register_blueprint(main_blueprint)
-from app.authenticate import authenticate as authenticate_blueprint
 app.register_blueprint(authenticate_blueprint, url_prefix='/authenticate')
--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,15 @@
 from flask import render_template, request,redirect,url_for,abort
 from . import main
 from flask_login import  login_required, current_user
-from app.templates.models import  User, PitchCategory, Pitches, Comments, Likes, Dislikes
+from app.template.models import  User, PitchCategory, Pitches, Comments, Likes, Dislikes
 from .forms import ProfileUpdate, PitchForm, CommentForm, CategoriesForm
 from .. import db,photos
 
 @main.route('/')
 def index():
-    ""
+    """
     A views root page funcction that returns index page and the various news source
-    ""
+    """
 
     categories = PitchCategory.get_categories()
 
