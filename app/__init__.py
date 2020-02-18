ef0ac0ff000a63b1324a383b101536e648972278
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-18 05:41:10 +0300

    creted a function for create_app with the config name and in it initialized bootstrap, db, logins and mail

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_moment import Moment
+
+def create_app(config_name):
+    app Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+	bootstrap.init_app(app)
+	db.init_app(app)
+	login_manager.init_app(app)
+	moment.init_app(app)
+	login_manager.session_protection = 'strong'
+	login_manager.login_view = 'auth.login'
+	configure_uploads(app,photos)
+	mail.init_app(app)
