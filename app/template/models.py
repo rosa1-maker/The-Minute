4e27105ea3652714ba602f669de36407ff8d2672
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-18 05:05:04 +0300

    created a class PitchCategory with id name and description of the pitches

--- app/template/models.py
+++ app/template/models.py
@@ -8,4 +8,39 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(init(user_id))
 
-    
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pitches = db.relationship("Pitches", backref="user", lazy="dynamic")
+    comment = db.relationship("Comments", backref="user", lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('Cannot read password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return 'User{self.username}'
+
+class PitchesCategory(db,Model):
+    '''
+    this class defines a pitch per category
+    '''
+    _tablename__ = 'pitch_categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+
+
