cfa170eacc6a3c46cf1a6a11a878bf8d52885e2e
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-21 10:24:31 +0300

    added a pitchform and change the html for index and base

--- app/main/form.py
+++ app/main/form.py
@@ -8,3 +8,13 @@ from app.models import User
 class ProfileUpdate():
     bio = TextAreaField('As a User', validators=[Required()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title', validators=[Required()])
+    body = TextAreaField('Enter your comment here', validators=[Required()])
+    author = StringField('Author', validators=[Required()])
+    category = RadioField('Pick Category',
+                          choices=[('business', 'business'),
+                                   ('jobs', 'jobs')],
+                          validators=[Required()])
+    submit = SubmitField('Submit')
