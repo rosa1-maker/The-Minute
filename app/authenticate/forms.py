576a4bc3bf19fb43d777fb588ceab052691ec5ba
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-21 10:26:53 +0300

    added validation for registration form

--- app/authenticate/forms.py
+++ app/authenticate/forms.py
@@ -5,26 +5,35 @@ from wtforms.validators import Required, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    Email=StringField('Email')
-    username=StringField('Username', validators=[Required()])
-    password = PasswordField('password', validators=[Required()] )
+    Email = StringField('Email')
+    username = StringField('Username', validators=[Required()])
+    password = PasswordField('password', validators=[Required()])
 
     submit = SubmitField('Signup')
     confirm = PasswordField('Repeat Password')
     accept_tos = BooleanField('I accept the TOS')
 
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError(
+                'There is already an account with that email. Check spelling or try another email')
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('That username is taken. Try another one')
+
+
 class LoginForm(FlaskForm):
-    Username=StringField('username', validators=[Required()])
-    password = PasswordField('password',validators=[Required()])
+    Username = StringField('username', validators=[Required()])
+    password = PasswordField('password', validators=[Required()])
 
     submit = SubmitField('login')
     confirm = PasswordField('Repeat Password')
     accept_tos = BooleanField('I accept the TOS')
 
 
-
 class Comment(FlaskForm):
 
-    title = StringField('The Pitch',validators=[Required()])
+    title = StringField('The Pitch', validators=[Required()])
     Comment = TextAreaField('Pitch Comment', validators=[Required()])
     submit = SubmitField('Submit')
